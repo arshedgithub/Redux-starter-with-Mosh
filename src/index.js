00453da4120d307251926fd747bcc08db5a5564e
arshedgithub <arshedahmedmail@gmail.com>
2021-11-02 10:54:56 +0530

    subscribe redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import store from "./store";
 
-console.log(store);
+store.subscribe(() => {
+  console.log("store changed!", store.getState());
+});
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
+
+console.log(store.getState());
